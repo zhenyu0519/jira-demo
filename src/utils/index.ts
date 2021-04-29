3e5ae77c25ed539f7f6f22cae073e85128f07a36
zhenyu0519 <zhenyu051988@gmail.com>
2021-04-29 02:05:37 -0700

    chore: change js jsx files to ts and tsx files

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,26 +1,27 @@
 import { useEffect, useState } from "react";
-import { clearTimeout } from "timers";
 
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
 
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
-  }, []);
+  });
 };
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     let timeout = window.setTimeout(() => setDebouncedValue(value), delay);
